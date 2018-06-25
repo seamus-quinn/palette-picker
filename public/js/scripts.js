c04c6313d1fe83e2be61b5268e1da10409aefd2d
Seamus Quinn <sumaesquinn@gmail.com>
2018-06-25 14:23:16 -0600

    Hook up front end js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,7 @@
 const paletteGenerator = document.querySelector('.palette-generator')
 
-const foo = $('.palette-generator')[0];
-console.log(foo)
-console.log(paletteGenerator)
-
 const generatePalette = () => {
   console.log('wooo')
 }
 
-foo.addEventListener('click', generatePalette)
+paletteGenerator.addEventListener('click', generatePalette)
