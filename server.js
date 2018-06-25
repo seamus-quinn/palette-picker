824703e7ab833213ba09db736b93943108772e71
Seamus Quinn <sumaesquinn@gmail.com>
2018-06-25 13:48:07 -0600

    Serve static html page

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Palette Picker';
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
 
 })
