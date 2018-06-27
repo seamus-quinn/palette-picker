6f37603d827193e213bd02035ef062dac1096453
Seamus Quinn <sumaesquinn@gmail.com>
2018-06-27 16:06:08 -0600

    Add post palette functionality

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ app.get('/api/v1/projects', (request, response) => {
     })
 })
 
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+    .then(palettes => {
+      response.status(200).json(palettes)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
 app.post('/api/v1/projects', (request, response) => {
   const project = request.body;
 
@@ -37,7 +47,36 @@ app.post('/api/v1/projects', (request, response) => {
 
   database('projects').insert(project, 'id')
     .then(project => {
-      response.status(201).json({ id: project[0]})
+      response.status(201).json({ id: project[0] })
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
+app.post('/api/v1/palettes', (request, response) => {
+  const palette = request.body
+
+  for (let requiredParameter of ['name', 'color1', 'color2', 'color3', 'color4', 'color5', 'project_id']) {
+    if(!palette[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: {
+          name: <String>,
+          color1: <String>,
+          color2: <String>,
+          color3: <String>,
+          color4: <String>,
+          color5: <String>,
+          project_id: <Number>
+        } 
+        You're missing a "${requiredParameter}" property.`})
+    }
+  }
+
+  database('palettes').insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] })
     })
     .catch(error => {
       response.status(500).json({ error })
