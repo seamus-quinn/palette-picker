ea5cf0804aea003fbc28462585f0ac131fea69e0
Seamus Quinn <sumaesquinn@gmail.com>
2018-06-27 15:44:51 -0600

    Write get and post methods for projects

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Palette Picker';
 app.locals.projects = []
@@ -10,36 +14,34 @@ app.locals.palettes = []
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (request, response) => {
-  
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
 })
 
 app.post('/api/v1/projects', (request, response) => {
-  const id = Date.now().toString();
-  const { projectName } = request.body;
+  const project = request.body;
 
-  if (!projectName) {
-    response.status(422).send({
-      error: 'Please provide a name for the project you wish to save'
-    });
-  } else {
-    app.locals.projects.push({ id, projectName })
-    response.status(201).json({ id, projectName })
+  for (let requiredParameter of ['name']) {
+    if(!project[requiredParameter]) {
+      return response
+        .status(422)
+        .send({ error: `Expected format: { name: <String> }. You're missing a "${requiredParameter}" property.`})
+    }
   }
-})
 
-app.post('/api/v1/palettes', (request, response) => {
-  const id = Date.now().toString();
-  const { paletteName } = request.body;
-
-  if (!paletteName) {
-    response.status(422).send({
-      error: 'Please provide a name for the palette you wish to save'
-    });
-  } else {
-    app.locals.palettes.push({ id, paletteName })
-    response.status(201).json({ id, paletteName })
-  }
+  database('projects').insert(project, 'id')
+    .then(project => {
+      response.status(201).json({ id: project[0]})
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
 })
 
 app.listen(app.get('port'), () => {
